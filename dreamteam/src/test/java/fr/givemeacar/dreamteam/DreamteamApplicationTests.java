eff293a77e44cd64fcfa429d86bddd76dd62bf9b
GinoChasles <gino.chasles@gmail.com>
2020-11-18 16:49:24 +0100

    init

---
+++ dreamteam/src/test/java/fr/givemeacar/dreamteam/DreamteamApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.givemeacar.dreamteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
