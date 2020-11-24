f4b579765e03a2a439cb68fb04e6d8396070ce38
Vivien Le Duc <leducvivien@gmail.com>
2020-11-24 12:52:26 +0100

    mise en place des entities

--- src/main/java/fr/givemeacar/app/model/Rent.java
+++ src/main/java/fr/givemeacar/app/model/Rent.java
@@ -1,22 +1,9 @@
 package fr.givemeacar.app.model;
 
-import java.time.LocalDate;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotBlank;
-
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Rent {
