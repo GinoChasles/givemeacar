8a4fe1a098baf17d0ef212a7275c96e6fe4ccd44
Vivien Le Duc <leducvivien@gmail.com>
2020-11-26 17:39:46 +0100

    services

--- src/main/java/fr/givemeacar/app/repository/CreditCardRepository.java
+++ src/main/java/fr/givemeacar/app/repository/CreditCardRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CreditCardRepository extends JpaRepository<Country, Integer> {
+public interface CreditCardRepository extends JpaRepository<CreditCard, Integer> {
 
 }
