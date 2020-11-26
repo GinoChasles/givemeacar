771301587c9a35978f19dc45c2636279a937c642
Vivien Le Duc <leducvivien@gmail.com>
2020-11-26 17:22:12 +0100

    repositories

--- src/main/java/fr/givemeacar/app/repository/UserStatusRepository.java
+++ src/main/java/fr/givemeacar/app/repository/UserStatusRepository.java
@@ -1,12 +1,12 @@
 package fr.givemeacar.app.repository;
 
-import fr.givemeacar.app.model.userStatus;
+import fr.givemeacar.app.model.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface userStatusRepository extends JpaRepository<userStatus, Integer> {
+public interface UserStatusRepository extends JpaRepository<UserStatus, Integer> {
 
 }
