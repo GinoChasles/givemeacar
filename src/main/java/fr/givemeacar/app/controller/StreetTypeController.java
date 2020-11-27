49241c8e38d21eac79399eded3a643851709bea9
Trystan VALENTIN <trystan.valentin@gmail.com>
2020-11-27 11:37:30 +0100

    ajout du / devant api dans les controllers

--- src/main/java/fr/givemeacar/app/controller/StreetTypeController.java
+++ src/main/java/fr/givemeacar/app/controller/StreetTypeController.java
@@ -10,7 +10,7 @@ import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api")
 public class StreetTypeController {
 
     @Autowired
