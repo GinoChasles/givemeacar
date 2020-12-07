258a8aa7bb3e50c4a35819d11d1b5ed962552a30
Vivien Le Duc <leducvivien@gmail.com>
2020-12-07 16:34:36 +0100

    Mise en place des sessions

--- src/main/java/fr/givemeacar/app/repository/AdministratorRepository.java
+++ src/main/java/fr/givemeacar/app/repository/AdministratorRepository.java
@@ -1,6 +1,12 @@
 package fr.givemeacar.app.repository;
 
 import fr.givemeacar.app.model.Administrator;
+import fr.givemeacar.app.model.UserStatus;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AdministratorRepository extends BaseCrudRepository<Administrator> {
+    @Query("SELECT c.userStatus FROM Administrator c WHERE c.mail = :mail AND c.password = :password")
+    public UserStatus findUserStatusByMailAndPassword(@Param("mail") String mail, @Param("password") String password);
+
 }
