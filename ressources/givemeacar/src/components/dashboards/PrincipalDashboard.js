94b1764c62a1d094d4a43597c4de697c4a2ef7e6
ginochasles <ginochasles@gmail.com>
2020-12-16 13:15:08 +0100

    13:15 16/12/2020

--- ressources/givemeacar/src/components/dashboards/PrincipalDashboard.js
+++ ressources/givemeacar/src/components/dashboards/PrincipalDashboard.js
@@ -1,7 +1,9 @@
 import * as React from "react";
 import AdminDashboard from './AdminDashboard'
+import ClientDashboard from "./ClientDashboard";
+import ManagerDashboard from "./ManagerDashboard";
 const PrincipalDashboard = () => {
     return <AdminDashboard />
 }
 
-  export default PrincipalDashboard;
+  export default PrincipalDashboard
