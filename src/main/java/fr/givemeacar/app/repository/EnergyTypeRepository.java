bded8579fa730d1196867bd03caa9f82f20e4776
Vivien Le Duc <leducvivien@gmail.com>
2020-12-16 15:01:06 +0100

    Les retours de requete respectent l'offset et la limite

--- src/main/java/fr/givemeacar/app/repository/EnergyTypeRepository.java
+++ src/main/java/fr/givemeacar/app/repository/EnergyTypeRepository.java
@@ -1,7 +1,13 @@
 package fr.givemeacar.app.repository;
 
+import fr.givemeacar.app.model.Agency;
 import fr.givemeacar.app.model.EnergyType;
+import org.springframework.data.jpa.repository.Query;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 public interface EnergyTypeRepository extends findByNameStartingWithRepository<EnergyType>,
         BaseCrudRepository<EnergyType> {
+
 }
