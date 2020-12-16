270d1bb356c1790cba762203b5b064c40f9f9f29
Vivien Le Duc <leducvivien@gmail.com>
2020-12-16 20:33:46 +0100

    mise en forme temporaire de la navbar car j'en avais marre de la voir comme ça

--- ressources/givemeacar/src/components/NavBar.js
+++ ressources/givemeacar/src/components/NavBar.js
@@ -1,6 +1,27 @@
 import React from "react";
 import {Link} from "react-router-dom"
 
-const NavBar = ({links}) => <ul>{links.map(link=><li><Link to={link.url}>{link.label}</Link></li>)}</ul>
+const listStyle = {
+    backgroundColor:'black',
+    padding:'1rem'
+}
+
+const itemStyle = {
+    listStyleType : 'none',
+    display:'inline-block',
+    paddingLeft:'1rem',
+    paddingRight:'1rem',
+}
+
+const linkStyle = {
+    textDecorationLine:'none',
+    color:'white'
+}
+
+const NavBar = ({links}) => <ul style={listStyle}>
+    {links.map(link=><li style={itemStyle}>
+        <Link to={link.url} style={linkStyle}>{link.label}</Link>
+    </li>)}
+</ul>
 
 export default NavBar
