16699131d0fdf0f5b028990a0b6942556d05613c
Vivien Le Duc <leducvivien@gmail.com>
2021-02-04 13:41:15 +0100

    icones

--- ressources/givemeacar/src/components/dashboards/AdminDashboard.js
+++ ressources/givemeacar/src/components/dashboards/AdminDashboard.js
@@ -72,7 +72,9 @@ import {
 
 
 import {
-  Palette, DirectionsCar, LocationCity, Explore, Streetview, Face, AirportShuttle, HomeWork, Receipt, Description, EvStation, Public, SupervisorAccount, Person
+  Palette, DirectionsCar, LocationCity, Explore, Streetview, Face,
+  AirportShuttle, HomeWork, Receipt, Description, EvStation, Public,
+  VpnKey, Person, RecordVoiceOver, FormatListNumbered, Map, LocalShipping
 } from '@material-ui/icons';
 
 import redirect from '../../lib/redirectIfNoSession'
@@ -86,33 +88,15 @@ const AdminDashboard = ({ dataProvider, i18nProvider }) => {
     { redirect('/')}
 
     <Admin dataProvider={dataProvider} i18nProvider={i18nProvider}>
-
-      <Resource icon={Palette} options={{ label: t("custom.colors") }} name="colors"
-        list={DisplayColor} show={ShowColor} edit={EditColor} create={CreateColor} />
-
-      <Resource icon={LocationCity} options={{ label: t("custom.cities") }} name="cities"
-        list={DisplayCity} show={ShowCity} edit={EditCity} create={CreateCity} />
-
-      <Resource icon={Explore} options={{ label: t("custom.departments") }} name="departments"
-        list={DisplayDepartment} show={ShowDepartment} edit={EditDepartment} create={CreateDepartment} />
-
-      <Resource icon={SupervisorAccount} options={{ label: t("custom.managers") }} name="managers"
-        list={DisplayManager} show={ShowManager} edit={EditManager} create={CreateManager} />
-
+      
       <Resource icon={Face} options={{ label: t("custom.clients") }} name="clients"
         list={DisplayClient} show={ShowClient} edit={EditClient} create={CreateClient} />
 
-      <Resource icon={SupervisorAccount} options={{ label: t("custom.administrators") }} name="administrators"
+      <Resource icon={RecordVoiceOver} options={{ label: t("custom.managers") }} name="managers"
+        list={DisplayManager} show={ShowManager} edit={EditManager} create={CreateManager} />
+
+      <Resource icon={VpnKey} options={{ label: t("custom.administrators") }} name="administrators"
         list={DisplayAdministrators} show={ShowAdministrators} edit={EditAdministrators} create={CreateAdministrators} />
-
-      <Resource icon={DirectionsCar} options={{ label: t("custom.cars") }} name="cars"
-        list={DisplayCar} show={ShowCar} edit={EditCar} create={CreateCar} />
-
-      <Resource icon={AirportShuttle} options={{ label: t("custom.brands") }} name="brands"
-        list={DisplayBrand} show={ShowBrand} edit={EditBrand} create={CreateBrand} />
-
-      <Resource icon={AirportShuttle} options={{ label: t("custom.models") }} name="models"
-        list={DisplayModel} show={ShowModel} edit={EditModel} create={CreateModel} />
 
       <Resource icon={HomeWork} options={{ label: t("custom.agencies") }} name="agencies"
         list={DisplayAgency} show={ShowAgency} edit={EditAgency} create={CreateAgency} />
@@ -123,23 +107,48 @@ const AdminDashboard = ({ dataProvider, i18nProvider }) => {
       <Resource icon={Description} options={{ label: t("custom.rents") }} name="rents"
         list={DisplayRent} show={ShowRent} edit={EditRent} create={CreateRent} />
 
-      <Resource icon={EvStation} options={{ label: t("custom.energyTypes") }} name="energy_types"
-        list={DisplayEnergyType} show={ShowEnergyType} edit={EditEnergyType} create={CreateEnergyType} />
 
-      <Resource icon={Explore} options={{ label: t("custom.regions") }} name="regions"
-        list={DisplayRegion} show={ShowRegion} edit={EditRegion} create={CreateRegion} />
 
-      <Resource icon={DirectionsCar} options={{ label: t("custom.suffices") }} name="street_suffices"
+      <Resource icon={DirectionsCar} options={{ label: t("custom.cars") }} name="cars"
+        list={DisplayCar} show={ShowCar} edit={EditCar} create={CreateCar} />
+
+
+
+      <Resource icon={FormatListNumbered} options={{ label: t("custom.suffices") }} name="street_suffices"
         list={DisplaySuffix} show={ShowSuffix} edit={EditSuffix} create={CreateSuffix} />
 
       <Resource icon={Streetview} options={{ label: t("custom.streets") }} name="streets"
         list={DisplayStreet} show={ShowStreet} edit={EditStreet} create={CreateStreet} />
 
+      <Resource icon={LocationCity} options={{ label: t("custom.cities") }} name="cities"
+        list={DisplayCity} show={ShowCity} edit={EditCity} create={CreateCity} />
+
+      <Resource icon={Explore} options={{ label: t("custom.departments") }} name="departments"
+        list={DisplayDepartment} show={ShowDepartment} edit={EditDepartment} create={CreateDepartment} />
+
+      <Resource icon={Map} options={{ label: t("custom.regions") }} name="regions"
+        list={DisplayRegion} show={ShowRegion} edit={EditRegion} create={CreateRegion} />
+
       <Resource icon={Public} options={{ label: t("custom.countries") }} name="countries"
         list={DisplayCountry} show={ShowCountry} edit={EditCountry} create={CreateCountry} />
 
+
+
+      <Resource icon={AirportShuttle} options={{ label: t("custom.brands") }} name="brands"
+        list={DisplayBrand} show={ShowBrand} edit={EditBrand} create={CreateBrand} />
+
+      <Resource icon={LocalShipping} options={{ label: t("custom.models") }} name="models"
+        list={DisplayModel} show={ShowModel} edit={EditModel} create={CreateModel} />
+
       <Resource icon={Person} options={{ label: t("custom.statuses") }} name="user_statuses"
         list={DisplayStatus} show={ShowStatus} edit={EditStatus} create={CreateStatus} />
+
+      <Resource icon={EvStation} options={{ label: t("custom.energyTypes") }} name="energy_types"
+        list={DisplayEnergyType} show={ShowEnergyType} edit={EditEnergyType} create={CreateEnergyType} />
+
+      <Resource icon={Palette} options={{ label: t("custom.colors") }} name="colors"
+        list={DisplayColor} show={ShowColor} edit={EditColor} create={CreateColor} />
+
     </Admin>
   </>
 }
